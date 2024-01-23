c5953e493250b87dfe705ebe7be29ea43e3fa7e7
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-23 14:40:32 +0500

    Update OIBDA.R
    Increased script functionality:
    
    • Added option to get all OIBDA components in one data frame together with OIBDA value

--- OIBDA.R
+++ OIBDA.R
@@ -1,11 +1,11 @@
 library("rvest") # Library
 
-OIBDA <- function(x){ # function to get OIBDA ratios
+OIBDA <- function(x, full = F){ # function to get OIBDA ratios
   
   oibda <- NULL # List for OIBDA values
   
   for (q in 1:length(x)){ a <- x[q] # Each ticker in vector
-  
+    
     is <- sprintf("https://finance.yahoo.com/quote/%s/financials?p=%s", a, a)
     
     page.is <- read_html(is) # Read HTML & extract necessary info
@@ -21,13 +21,21 @@ OIBDA <- function(x){ # function to get OIBDA ratios
     
     for (m in 1:length(p)){ c <- rbind(c, y[grep(p[m], y) + 1][1]) }
     
-    c <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", c)) 
+    c <- as.numeric(gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", c)))
     
-    oibda <- rbind(oibda, sum(as.numeric(c[seq(4)]))) } # OIBDA
+    if (isTRUE(full)){ w <- cbind(sum(c[seq(4)]), c[1], c[2], c[3], c[4])
+      
+      colnames(w) <- c("OIBDA", "Operating Income", "Tax Provision",
+                       "Interest Expense", "Reconciled Depreciation") 
+      
+      oibda <- rbind(oibda, w) } else {
+        
+        oibda <- rbind(oibda, sum(as.numeric(c[seq(4)]))) } } # OIBDA
     
   rownames(oibda) <- x # Ticker names
-  colnames(oibda) <- "OIBDA" # Column Name
+  
+  if (isFALSE(full)){ colnames(oibda) <- "OIBDA"}
   
   oibda # Display
 }
-OIBDA(c("AAPL", "MSFT", "AMZN", "GOOGL", "META", "NVDA")) # Test
+OIBDA(c("AAPL", "MSFT", "AMZN", "GOOGL", "META", "NVDA"), full = T) # Test
